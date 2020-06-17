aa063e6307861be0d9a68c522a391c9f487183dc
Olha Danylchenko <danylchenkoolha@gmail.com>
2020-06-17 14:57:53 +0200

    create+delete routes for friendship

--- routes/friendships.js
+++ routes/friendships.js
@@ -5,21 +5,36 @@ const User = require("../models/User");
 const Friendship = require("../models/Friendship");
 const userService = require("../services/user.service");
 
-router.get(
-  "/",
-  /*middleware goes here?,*/ function (req, res) {
-    console.log(req, "friendships");
-    Friendship.find({
-      $or: [{ friend1: req.user.id }, { friend2: req.user.id }],
-    }).then((friendship) => res.json(friendship));
-  }
-);
+router.get("/", function (req, res) {
+  Friendship.find({
+    $or: [{ friend1: req.user.id }, { friend2: req.user.id }],
+  }).then((friendship) => res.json(friendship));
+});
 
 router.post(
   "/:id",
   /*middleware goes here?,*/ function (req, res) {
-    //console.log(req.body);
+    return Friendship.create({
+      friend1: req.user.id,
+      friend2: req.params.id,
+    })
+      .then((friendship) => {
+        res.status(200).json({ friendship });
+      })
+      .catch((err) => {
+        res.json(err);
+      });
   }
 );
+
+router.delete("/:id", function (req, res) {
+  Friendship.findByIdAndDelete(req.params.id)
+    .then((friendship) => {
+      res.status(200).json({ message: "ok" });
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
 
 module.exports = router;
