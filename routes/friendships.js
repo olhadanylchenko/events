a0fbdf09aab45bb75eccf3ced735e0434d298761
Olha Danylchenko <danylchenkoolha@gmail.com>
2020-06-18 10:46:22 +0200

    reformatted to async

--- routes/friendships.js
+++ routes/friendships.js
@@ -1,40 +1,34 @@
 const express = require("express");
 const router = express.Router();
 
-const User = require("../models/User");
 const Friendship = require("../models/Friendship");
-const userService = require("../services/user.service");
 
-router.get("/", function (req, res) {
-  Friendship.find({
+router.get("/", async function (req, res) {
+  const friendship = await Friendship.find({
     $or: [{ friend1: req.user.id }, { friend2: req.user.id }],
-  }).then((friendship) => res.json(friendship));
+  });
+  res.json(friendship);
 });
 
-router.post(
-  "/:id",
-  /*middleware goes here?,*/ function (req, res) {
-    return Friendship.create({
+router.post("/:id", async function (req, res) {
+  try {
+    const friendship = await Friendship.create({
       friend1: req.user.id,
       friend2: req.params.id,
-    })
-      .then((friendship) => {
-        res.status(200).json({ friendship });
-      })
-      .catch((err) => {
-        res.json(err);
-      });
-  }
-);
-
-router.delete("/:id", function (req, res) {
-  Friendship.findByIdAndDelete(req.params.id)
-    .then((friendship) => {
-      res.status(200).json({ message: "ok" });
-    })
-    .catch((err) => {
-      res.json(err);
     });
+    res.status(200).json({ friendship });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.delete("/:id", async function (req, res) {
+  try {
+    await Friendship.findByIdAndDelete(req.params.id);
+    res.status(200).json({ message: "ok" });
+  } catch (err) {
+    res.json(err);
+  }
 });
 
 module.exports = router;
