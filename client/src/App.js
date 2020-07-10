63bc2c1ab8ba613515d355c2bda25d43e3dd7386
Olha Danylchenko <danylchenkoolha@gmail.com>
2020-07-10 10:40:21 +0200

    initial frontend

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import "./components/Login";
+import Login from "./components/Login";
+
+function App() {
+  return <Login />;
+}
+
+export default App;
---
+++ client/src/components/Login.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Login() {
+  const handleSubmit = () => {
+    console.log("Hi!");
+  };
+  return (
+    <>
+      <h1>Hi!</h1>
+    </>
+  );
+}
