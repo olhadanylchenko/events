4f1eff18ffdc2973b55e913d9050b0316a97aaf2
Olha Danylchenko <danylchenkoolha@gmail.com>
2020-07-10 11:25:42 +0200

    set proxy, request is incorrect

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 export default function Login() {
-  const handleSubmit = () => {
-    console.log("Hi!");
+  const [token, setToken] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleEmail = (event) => {
+    const email = event.target.value;
+    setEmail(email);
+  };
+  const handlePassword = (event) => {
+    const password = event.target.value;
+    setPassword(password);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios.post("/users/authenticate", { email, password }).then((res) => {
+      console.log(res);
+    });
   };
   return (
     <>
       <h1>Hi!</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="email">Email</label>
+        <input type="text" id="email" value={email} onChange={handleEmail} />
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={handlePassword}
+        />
+        <button type="submit">GO</button>
+      </form>
     </>
   );
 }
