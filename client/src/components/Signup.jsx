0766990e503aa00b2f9fec011bb5714cb811d147
Olha Danylchenko <danylchenkoolha@gmail.com>
2020-07-10 17:15:12 +0200

    looging the error message from the backend woohoo!

--- client/src/components/Signup.jsx
+++ client/src/components/Signup.jsx
@@ -1,11 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 export default function Signup() {
-  const handleSubmit = () => {};
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [birthDate, setBirthDate] = useState("");
+
+  const handleUsername = (e) => {
+    setUsername(e.target.value);
+  };
+  const handleEmail = (e) => {
+    setEmail(e.target.value);
+  };
+  const handlePassword = (e) => {
+    setPassword(e.target.value);
+  };
+  const handleBirthDate = (e) => {
+    setBirthDate(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post("/api/users/register", { username, email, password, birthDate })
+      .then((res) => console.log(res))
+      .catch((err) => {
+        console.log(err.response.data.message);
+      });
+  };
   return (
     <>
       <h1>Hi!</h1>
       <form onSubmit={handleSubmit}>
+        <label htmlFor="username">Username</label>
+        <input
+          type="username"
+          id="username"
+          value={username}
+          onChange={handleUsername}
+        />
         <label htmlFor="email">Email</label>
         <input type="text" id="email" value={email} onChange={handleEmail} />
         <label htmlFor="password">Password</label>
@@ -15,6 +49,14 @@ export default function Signup() {
           value={password}
           onChange={handlePassword}
         />
+        <label htmlFor="birthDate">Birth date</label>
+        <input
+          type="date"
+          id="birthDate"
+          value={birthDate}
+          onChange={handleBirthDate}
+        />
+
         <button type="submit">GO</button>
       </form>
     </>
